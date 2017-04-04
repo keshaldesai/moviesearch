787c76c7a1f2c07d084901315dbb40a6219b7a72
Keshal Desai <novalearngood@gmail.com>
2017-04-03 22:04:55 -0400

    border fix, opacity change, header margin

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ export const FETCH_MOVIE = 'FETCH_MOVIE';
 const API_KEY = 'f7a6b4be5b81c46de9945d56e3165355';
 
 export function fetchMovie() {
-	const request = axios.get(`https://api.themoviedb.org/3/movie/157336?api_key=${API_KEY}`);
+	const request = axios.get(`https://api.themoviedb.org/3/movie/198184?api_key=${API_KEY}`);
 	return {
 		type: FETCH_MOVIE,
 		payload: request
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
 	return (
 		<div className="header">
 			<img className="logo" src={logo} alt="Powered by The Movie DB" />
-			<span className="search-box"><i className="fa fa-search fa-2x" aria-hidden="true"></i><input className="search-input" type="text" name="search" placeholder="Search Movie Title..." /></span>
+			<span className="search-box"><i className="fa fa-search fa-2x" aria-hidden="true"></i><input className="search-input" type="text" name="search" placeholder="Search Movie Title..." autoComplete="off" /></span>
 		</div>
 	)
 }
